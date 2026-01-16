80bc8a3981304a901ffd78d63cf0c32a1768ddee
adelrioj01 <adelrioj01@gmail.com>
2026-01-16 16:59:53 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,10 @@ resource "google_bigquery_dataset" "analytics" {
   dataset_id = "housing_analytics"
   location   = "EU"
 }
+
+resource "google_storage_bucket" "raw_data" {
+  name     = "${var.project_id}-housing-raw"
+  location = "EU"
+
+  uniform_bucket_level_access = true
+}
